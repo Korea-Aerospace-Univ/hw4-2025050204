868d808eae600f7a02336100c5cdd0dc9cfed9dd
2025050204 <choiseoyoung@kau.kr>
2026-04-15 15:59:52 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-int main(void){
-    int a, b, i=1; 
+int main(){
+    int a, b, i=0;
     scanf("%d", &a);
-    do{
-        scanf("%d", &b);
-        i++;
 
+    do{
+        scanf("%d", &a);
+        i++;
+        
         if(a<b){
-            printf("%d > ?\n", b);
+            printf("%d>?\n", b);
         }
         else if(a>b){
-            printf("%d < ?\n", b);
+            printf("%d<?\n", b);
         }
         else{
-            printf("%d == ?\n", b);
-            printf("%d", i);
+            printf("%d==?\n", b);
         }
-    } while(a!=b);
-    
+    }while(a!=b);
+    printf("%d", i);
     return 0;
 }
