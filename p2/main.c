ddc30be511a5b49846ccaaf6d24acddd86cab7ce
2025050204 <choiseoyoung@kau.kr>
2026-04-15 16:45:20 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
 
-int main(void){
+int main(void) {
     int n;
-    char answkd;
+    char c; 
 
     scanf("%d", &n);
-    scanf("%c", answkd);
 
-    int maxs=0, maxc=0; // 각각 제일 많은 소문자, 숫자 의미 
-    int nows=0, nowc=0; // 각각 지금 세고 있는 소문자, 숫자
+    int maxs = 0, maxc = 0;
+    int nows = 0, nowc = 0;
 
-    for(int i = 0; i < n; i++){
-        if (answkd >= 'a' && answkd <= 'z'){
+    for (int i = 0; i < n; i++) {
+        scanf(" %c", &c); 
+        if (c >= 'a' && c <= 'z') {
             nows++;
-            nowc=0;
-            if(nows>maxs){
-                maxs=nows;
-            }
-        }
-        else if(answkd >= '0' && answkd<= '9'){
-            nows=0;
+            nowc = 0;
+            if (nows > maxs) maxs = nows;
+        } 
+        else if (c >= '0' && c <= '9') {
             nowc++;
-            if(nowc>maxc){
-                maxc=nowc;
-            }
-        }
-        else{
-            nows=0;
-            nowc=0;
+            nows = 0;
+            if (nowc > maxc) maxc = nowc;
+        } 
+        else {
+            nows = 0;
+            nowc = 0;
         }
     }
+
     printf("%d\n", maxs);
-    printf("%d", maxc);
+    printf("%d\n", maxc);
+
+    return 0;
 }
