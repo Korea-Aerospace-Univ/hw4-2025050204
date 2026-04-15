18384424bd814ffec9d0645cea74f39dd9b9030b
2025050204 <choiseoyoung@kau.kr>
2026-04-15 15:26:08 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 int main(void){
-    int a=5, b, i=1; // a는 5(정답), b는 입력받을 수, i는 횟수
+    int a=5, b, i=0; 
     do{
         scanf("%d", &b);
         i++;
 
         if(a<b){
-            printf("%d > ?\n", b);
+            printf("%d>?\n", b);
         }
         else if(a>b){
-            printf("%d < ?\n", b);
+            printf("%d<?\n", b);
         }
         else{
-            printf("%d == ?\n", b);
+            printf("%d==?\n", b);
             printf("%d", i);
         }
     } while(a!=b);
